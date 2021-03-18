2d7b559814869bb8d7138c932cda636795877ea0
AnatoliiMikhov <anatoli.mihov@gmail.com>
2021-03-19 00:25:53 +0200

    accordion fix repo

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,5 +1,4 @@
 const accordion = (triggersSelector, contentSelector) => {
-<<<<<<< HEAD
   const btns = document.querySelectorAll(triggersSelector),
     blocks = document.querySelectorAll(contentSelector);
 
@@ -26,35 +25,6 @@ const accordion = (triggersSelector, contentSelector) => {
       }
     });
   });
-=======
-	const btns = document.querySelectorAll(triggersSelector),
-		blocks = document.querySelectorAll(contentSelector);
-
-	btns.forEach(btn => {
-		btn.addEventListener('click', function () {
-
-			if (!this.classList.contains('active-style')) {
-				btns.forEach(btn => {
-					btn.classList.remove('active-style');
-				});
-
-				blocks.forEach(block => {
-					block.classList.remove('active-content');
-					block.style.maxHeight = '0';
-				});
-			}
-
-			this.classList.toggle('active-style');
-			this.nextElementSibling.classList.toggle('active-content');
-
-			if (this.classList.contains('active-style')) {
-				this.nextElementSibling.style.maxHeight = (this.nextElementSibling.scrollHeight + 80) + 'px';
-			} else {
-				this.nextElementSibling.style.maxHeight = '0';
-			}
-		});
-	});
->>>>>>> 225419b147f9b28b048093f87f29fdaeb9845153
 };
 
 export default accordion;
