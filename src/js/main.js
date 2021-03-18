079141406f93a9bab85f055d1a5f1ba346dd3474
AnatoliiMikhov <anatoli.mihov@gmail.com>
2021-03-18 22:49:14 +0200

    accordion ver1.0

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ import showMoreStyles from './modules/showMoreStyles';
 import calcCost from './modules/calcCost';
 import filter from './modules/filter';
 import pictureSize from './modules/pictureSize';
+import accordion from './modules/accordion';
 
 window.addEventListener('DOMContentLoaded', () => {
 	'use strict';
@@ -22,4 +23,5 @@ window.addEventListener('DOMContentLoaded', () => {
 	calcCost('#size', '#material', '#options', '.calc-form .promocode', '.calc-form .calc-price');
 	filter();
 	pictureSize('.sizes-block');
+	accordion('.accordion-heading', '.accordion-block');
 });
