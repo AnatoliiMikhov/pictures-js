225419b147f9b28b048093f87f29fdaeb9845153
AnatoliiMikhov <anatoli.mihov@gmail.com>
2021-03-18 23:45:18 +0200

    acordion ver2.0 checkout branch

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,21 +1,29 @@
-const accordion = (triggersSelector, itemsSelector) => {
+const accordion = (triggersSelector, contentSelector) => {
 	const btns = document.querySelectorAll(triggersSelector),
-		blocks = document.querySelectorAll(itemsSelector);
-
-	blocks.forEach(block => {
-		block.classList.add('animated', 'fadeInDown');
-	});
+		blocks = document.querySelectorAll(contentSelector);
 
 	btns.forEach(btn => {
-
 		btn.addEventListener('click', function () {
-			if (!this.classList.contains('active')) {
+
+			if (!this.classList.contains('active-style')) {
 				btns.forEach(btn => {
-					btn.classList.remove('active', 'active-style');
+					btn.classList.remove('active-style');
+				});
+
+				blocks.forEach(block => {
+					block.classList.remove('active-content');
+					block.style.maxHeight = '0';
 				});
 			}
 
-			this.classList.add('active', 'active-style');
+			this.classList.toggle('active-style');
+			this.nextElementSibling.classList.toggle('active-content');
+
+			if (this.classList.contains('active-style')) {
+				this.nextElementSibling.style.maxHeight = (this.nextElementSibling.scrollHeight + 80) + 'px';
+			} else {
+				this.nextElementSibling.style.maxHeight = '0';
+			}
 		});
 	});
 };
